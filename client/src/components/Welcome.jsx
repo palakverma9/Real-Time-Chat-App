a679b1e25cab6227b6e0da7375c1b2bb7d3697db
Palak verma <palakv23@iitk.ac.in>
2025-07-04 17:01:37 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,43 +1,32 @@
-
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Robot from "../assets/robotmy.gif";
-import Logout from "./Logout";
 
+const Welcome = ({ currentUser }) => {
+  const [userName, setUserName] = useState("");
 
+  useEffect(() => {
+    const userDataString = localStorage.getItem('chat-app-key');
+    if (!userDataString) {
+      setUserName(""); // or redirect to login
+      return;
+    }
+    const userData = JSON.parse(userDataString);
+    setUserName(userData?.username || "");
+  }, []);
 
-const Welcome = ({currentUser}) => {
-
-    const [userName, setUserName] = useState("");
-
-
-    useEffect(() => {
-        const fetchUserName = async () => {
-            const userData = await JSON.parse(localStorage.getItem('chat-app-key'));
-            setUserName(userData.username);
-        };
-    
-        fetchUserName();
-    }, []);
-
-    return (
-
-        <Container>
-
-            <img src={Robot} alt="" />
-            <h1>
-                Welcome, 
-                
-                <span>{currentUser?.username}!</span>
-            </h1>
-            <h3>Please select a chat to Start messaging.</h3>
-
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <img src={Robot} alt="" />
+      <h1>
+        Welcome, <span>{userName || currentUser?.username || "User"}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
+};
 
 export default Welcome;
-
 
 const Container = styled.div`
   display: flex;
